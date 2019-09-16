832178d8435ca7f56933d6fbcfbd64537d69ec42
teddyhn <tngo97@gmail.com>
2019-09-16 15:02:14 -0700

    Initial commit, added test for: 'defaults to unlocked and open'

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,13 @@
 // Test away!
+
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Display from './Display';
+
+  
+test('defaults to unlocked and open', () => {
+    const { getByText } = render(<Display />);
+    expect(getByText('Unlocked'));
+    expect(getByText('Open'));
+});
