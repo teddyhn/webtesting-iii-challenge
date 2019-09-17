3a9e7e9ce382097ae8ea9028b08c036a9f2a7afb
teddyhn <tngo97@gmail.com>
2019-09-16 20:11:02 -0700

    Finished writing tests for Controls component

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import { render } from '@testing-library/react';
-import 'jest-dom/extend-expect';
+import '@testing-library/jest-dom/extend-expect';
 
 import Display from './Display';
 
